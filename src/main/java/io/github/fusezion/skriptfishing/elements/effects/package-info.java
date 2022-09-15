4121ad64a1f442a41084280f8c008d265e8e1198
Fusezion <fusezionstream@gmail.com>
2022-09-14 23:26:27 -0400

    Removed pull effect
    Found out this caused a few issues, been removed.

--- src/main/java/io/github/fusezion/skriptfishing/elements/effects/package-info.java
+++
@@ -1,5 +0,0 @@
-@NonNullByDefault({DefaultLocation.PARAMETER, DefaultLocation.RETURN_TYPE, DefaultLocation.FIELD})
-package io.github.fusezion.skriptfishing.elements.effects;
-
-import org.eclipse.jdt.annotation.DefaultLocation;
-import org.eclipse.jdt.annotation.NonNullByDefault;
